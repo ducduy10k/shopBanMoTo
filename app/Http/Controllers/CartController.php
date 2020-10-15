7555a9eaf49b49e853f53b366f2232b34221163b
betapchoi10k <64733127+betapchoi10k@users.noreply.github.com>
2020-10-15 22:42:26 +0700

    new cart controller

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use DB;
+use Session ;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Redirect;
+session_start();
+class CartController extends Controller
+{
+       public function save_cart(Request $request){
+            $productId = request->
+            
+       }
+}
